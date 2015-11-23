5cc7eaaa4bb7430854fa22ff8dc3ab156ee8c294
Jorge Calado <jcalado@thing-pink.pt>
2015-11-23 16:35:19 +0000

    - Add Firebase support

---
+++ app/src/main/java/pink/instagram/fetchserver/utils/FirebaseUtil.java
@@ -0,0 +1,136 @@
+package pink.instagram.fetchserver.utils;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.FirebaseException;
+import com.firebase.client.ValueEventListener;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Jorge on 23/11/2015.
+ */
+public class FirebaseUtil {
+    private static final String firebaseRootURL = "https://gdgsummit2015.firebaseio.com";
+    private static FirebaseUtil singleton;
+    private Firebase summitFirebase;
+
+    /**
+     * lets keep this private, shall we?
+     * */
+    private FirebaseUtil(){
+        summitFirebase = new Firebase(firebaseRootURL);
+        registerFirebaseListener();
+    }
+
+    public static FirebaseUtil getInstance(Context applicationContext) {
+        if(singleton == null) {
+            Firebase.setAndroidContext(applicationContext.getApplicationContext());
+            singleton = new FirebaseUtil();
+        }
+
+        return singleton;
+    }
+
+    public void addUserphoto(final String username, final String photoUrl) {
+        summitFirebase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                SummitUserFirebaseUserModel userModel = null;
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    DataSnapshot userSnapshot = dataSnapshot.child(username);
+                    userModel = userSnapshot.getValue(SummitUserFirebaseUserModel.class);
+                }
+                if (userModel == null) {
+                    userModel = new SummitUserFirebaseUserModel();
+                    userModel.username = username;
+                    userModel.numberOfPhotos = 0;
+                    userModel.photosURLs = new ArrayList<String>();
+                }
+
+                userModel.numberOfPhotos++;
+
+                ArrayList<String> newURLsList = new ArrayList<String>();
+
+                newURLsList.addAll(userModel.getPhotosURLs());
+                newURLsList.add(photoUrl);
+                userModel.photosURLs = newURLsList;
+
+                summitFirebase.child("users").child(username).setValue(userModel);
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
+    }
+
+    private void registerFirebaseListener() {
+        Log.i("GNGTV", "Data changed");
+        summitFirebase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                Log.i("GNGTV", "Data changed");
+                try {
+                    SummitUserFirebaseModel userInfo = snapshot.getValue(SummitUserFirebaseModel.class);
+                    Log.i("GNGTV", "There is a user: " + Boolean.toString(userInfo != null));
+                    if (userInfo != null) {
+
+                    }
+                    //else
+                    //mCallback.error???
+                } catch (FirebaseException fe) {
+                    Log.e("GNGTV", "Error while parsing firebase snapshot.", fe);
+                }
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+                Log.i("GNGTV", "Error:" + firebaseError.getMessage());
+            }
+        });
+    }
+
+    public static class SummitUserFirebaseModel {
+        public SummitUserFirebaseUserModel users;
+    }
+
+    public static class SummitUserFirebaseUserModel {
+        private String username;
+        private int numberOfPhotos;
+        private ArrayList<String> photosURLs;
+
+        public SummitUserFirebaseUserModel() {
+
+        }
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public int getNumberOfPhotos() {
+            return numberOfPhotos;
+        }
+
+        public void setNumberOfPhotos(int numberOfPhotos) {
+            this.numberOfPhotos = numberOfPhotos;
+        }
+
+        public ArrayList<String> getPhotosURLs() {
+            return photosURLs;
+        }
+
+        public void setPhotosURLs(ArrayList<String> photosURLs) {
+            this.photosURLs = photosURLs;
+        }
+    }
+}
